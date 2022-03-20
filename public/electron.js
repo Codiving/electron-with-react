897e8e31837ba8f3efee006b7db7d1eae640fd60
Codiving <codivingmoon@gmail.com>
2022-03-20 17:49:24 +0900

    자동업데이트 추가

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,11 @@
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const isDev = require("electron-is-dev");
 const path = require("path");
+const { autoUpdater } = require("electron-updater");
+const ProgressBar = require("electron-progressbar");
 
 let mainWindow = null;
+let progressBar = null;
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
@@ -61,9 +64,74 @@ ipcMain.on("files", async event => {
   event.reply("files", { files: result });
 });
 
+// 자동으로 업데이트가 되는 것 방지
+autoUpdater.autoDownload = false;
+
+autoUpdater.on("checking-for-update", () => {
+  console.log("업데이트 확인 중");
+});
+
+autoUpdater.on("update-available", () => {
+  console.log("업데이트 버전 확인");
+
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "Update",
+      message:
+        "새로운 버전이 확인되었습니다. 설치 파일을 다운로드 하시겠습니까?",
+      buttons: ["지금 설치", "나중에 설치"]
+    })
+    .then(result => {
+      const { response } = result;
+
+      if (response === 0) autoUpdater.downloadUpdate();
+    });
+});
+
+autoUpdater.on("update-not-available", () => {
+  console.log("업데이트 불가");
+});
+
+autoUpdater.once("download-progress", () => {
+  console.log("설치 중");
+
+  progressBar = new ProgressBar({
+    text: "Download 합니다."
+  });
+
+  progressBar
+    .on("completed", () => {
+      console.log("설치 완료");
+    })
+    .on("aborted", () => {
+      console.log("aborted");
+    });
+});
+
+autoUpdater.on("update-downloaded", () => {
+  console.log("업데이트 완료");
+
+  progressBar.setCompleted();
+
+  dialog
+    .showMessageBox({
+      type: "Info",
+      title: "Update",
+      message: "새로운 버전이 다운로드 되었습니다. 다시 시작하시겠습니까?",
+      buttons: ["예", "아니오"]
+    })
+    .then(result => {
+      const { response } = result;
+
+      if (response === 0) autoUpdater.quitAndInstall(false, true);
+    });
+});
+
 // electron이 초기화 끝났을 때
 app.on("ready", () => {
   createWindow();
+  autoUpdater.checkForUpdates();
 });
 
 // 모든 window가 종료되었을 때
